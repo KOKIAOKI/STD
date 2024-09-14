993736912abf808226802bfc0e9d300928183109
KOKIAOKI <koki.aoki@map4.jp>
2024-09-14 19:54:02 +0900

    fix

--- demo/open_dataset_test.cpp
+++ demo/open_dataset_test.cpp
@@ -5,6 +5,51 @@
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
 
+struct PoseStamp {
+  double time;
+  Eigen::Vector3d trans;
+  Eigen::Quaterniond quat;
+
+  Eigen::Matrix4d getMatrix() const {
+    Eigen::Isometry3d T_temp = Eigen::Isometry3d::Identity();
+    T_temp.translation() = trans;
+    T_temp.linear() = quat.toRotationMatrix();
+    Eigen::Matrix4d T = T_temp.matrix();
+    return T;
+  }
+};
+
+std::vector<PoseStamp> readGroundTruth(const std::string& file_name) {
+  std::ifstream ifs(file_name);
+  std::string line;
+
+  std::vector<PoseStamp> gt_vec;
+
+  // skip first line
+  std::getline(ifs, line);
+  while (std::getline(ifs, line)) {
+    std::vector<std::string> data = split(line, ',');
+    std::vector<double> row;
+    for (int i = 0; i < data.size(); i++) {
+      row.push_back(std::stod(data[i]));
+    }
+
+    PoseStamp pose_stamp;
+    double time = row[0] + row[1] / 1e9;
+
+    pose_stamp.time = time;
+    pose_stamp.trans = Eigen::Vector3d(row[2], row[3], row[4]);
+
+    Eigen::Quaterniond quat(row[8], row[5], row[6], row[7]);
+    quat.normalize();
+    pose_stamp.quat = quat;
+
+    gt_vec.emplace_back(pose_stamp);
+  }
+
+  return gt_vec;
+}
+
 int main(int argc, char** argv) {
   ros::init(argc, argv, "demo_kitti");
   ros::NodeHandle nh;
@@ -37,30 +82,57 @@ int main(int argc, char** argv) {
   std::vector<double> update_time;
   int triggle_loop_num = 0;
 
+  // read ground truth
+  const auto gt_vec = readGroundTruth(pose_path);
+
   // file loop
   for () {
     // TODO get time from filename
-    double laser_time = ;
+    double time_stamp = ;
 
-    // TODO get nearest time from csv
+    // find gt pose stamp
+    auto min_error_iter = std::min_element(gt_vec.begin(), gt_vec.end(), [time_stamp](const PoseStamp& a, const PoseStamp& b) {
+      return std::abs(a.time - time_stamp) < std::abs(b.time - time_stamp);
+    });
+    int index = std::distance(gt_vec.begin(), min_error_iter);
+    auto gt_pose_stamp = gt_vec[index];
 
-    // TODO transform point cloud
-    pcl::PointCloud<pcl::PointXYZI> cloud;
+    if (time_stamp != gt_pose_stamp.time) {
+      std::cout << std::fixed << std::setprecision(18) << "[Missing]time_stamp: " << time_stamp << ", gt_pose_stamp.time: " << gt_pose_stamp.time << std::endl;
+      continue;
+    }
+
+    // pcl::PointCloud<pcl::PointXYZI> cloud;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::transformPointCloud(*cloud, *cloud, gt_pose_stamp.getMatrix();.cast<float>());
 
     // insert
     down_sampling_voxel(cloud, config_setting.ds_size_);
-    temp_cloud->points.insert(temp_cloud->points.end(), cloud->points.begin(), cloud->points.end());
-  }
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_xyzi(new pcl::PointCloud<pcl::PointXYZI>());
+    pcl::copyPointCloud(*cloud, *cloud_xyzi);
+    temp_cloud->points.insert(temp_cloud->points.end(), cloud_xyzi->points.begin(), cloud_xyzi->points.end());
 
-  // check if keyframe
-  if (cloudInd % config_setting.sub_frame_num_ == 0 && cloudInd != 0) {
-    std::cout << "Key Frame id:" << keyCloudInd << ", cloud size: " << temp_cloud->size() << std::endl;
     // step1. Descriptor Extraction
     auto t_descriptor_begin = std::chrono::high_resolution_clock::now();
     std::vector<STDesc> stds_vec;
     std_manager->GenerateSTDescs(temp_cloud, stds_vec);
     auto t_descriptor_end = std::chrono::high_resolution_clock::now();
     descriptor_time.push_back(time_inc(t_descriptor_end, t_descriptor_begin));
+
+    // step3. Add descriptors to the database
+    auto t_map_update_begin = std::chrono::high_resolution_clock::now();
+    std_manager->AddSTDescs(stds_vec);
+    auto t_map_update_end = std::chrono::high_resolution_clock::now();
+    update_time.push_back(time_inc(t_map_update_end, t_map_update_begin));
+
+    // TODO
+    pcl::PointCloud<pcl::PointXYZI> save_key_cloud;
+    save_key_cloud = *temp_cloud;
+    std_manager->key_cloud_vec_.push_back(save_key_cloud.makeShared());
+  }
+
+  // file loop
+  for () {
     // step2. Searching Loop
     auto t_query_begin = std::chrono::high_resolution_clock::now();
     std::pair<int, double> search_result(-1, 0);
@@ -76,21 +148,6 @@ int main(int argc, char** argv) {
     }
     auto t_query_end = std::chrono::high_resolution_clock::now();
     querying_time.push_back(time_inc(t_query_end, t_query_begin));
-
-    // step3. Add descriptors to the database
-    auto t_map_update_begin = std::chrono::high_resolution_clock::now();
-    std_manager->AddSTDescs(stds_vec);
-    auto t_map_update_end = std::chrono::high_resolution_clock::now();
-    update_time.push_back(time_inc(t_map_update_end, t_map_update_begin));
-    std::cout << "[Time] descriptor extraction: " << time_inc(t_descriptor_end, t_descriptor_begin) << "ms, "
-              << "query: " << time_inc(t_query_end, t_query_begin) << "ms, "
-              << "update map:" << time_inc(t_map_update_end, t_map_update_begin) << "ms" << std::endl;
-    std::cout << std::endl;
-
-    pcl::PointCloud<pcl::PointXYZI> save_key_cloud;
-    save_key_cloud = *temp_cloud;
-
-    std_manager->key_cloud_vec_.push_back(save_key_cloud.makeShared());
 
     // publish
 
@@ -118,27 +175,27 @@ int main(int argc, char** argv) {
     temp_cloud->clear();
     keyCloudInd++;
     loop.sleep();
-  }
 
-  nav_msgs::Odometry odom;
-  odom.header.frame_id = "camera_init";
-  odom.pose.pose.position.x = translation[0];
-  odom.pose.pose.position.y = translation[1];
-  odom.pose.pose.position.z = translation[2];
-  Eigen::Quaterniond q(rotation);
-  odom.pose.pose.orientation.w = q.w();
-  odom.pose.pose.orientation.x = q.x();
-  odom.pose.pose.orientation.y = q.y();
-  odom.pose.pose.orientation.z = q.z();
-  pubOdomAftMapped.publish(odom);
-  loop.sleep();
-  cloudInd++;
-  double mean_descriptor_time = std::accumulate(descriptor_time.begin(), descriptor_time.end(), 0) * 1.0 / descriptor_time.size();
-  double mean_query_time = std::accumulate(querying_time.begin(), querying_time.end(), 0) * 1.0 / querying_time.size();
-  double mean_update_time = std::accumulate(update_time.begin(), update_time.end(), 0) * 1.0 / update_time.size();
-  std::cout << "Total key frame number:" << keyCloudInd << ", loop number:" << triggle_loop_num << std::endl;
-  std::cout << "Mean time for descriptor extraction: " << mean_descriptor_time << "ms, query: " << mean_query_time << "ms, update: " << mean_update_time
-            << "ms, total: " << mean_descriptor_time + mean_query_time + mean_update_time << "ms" << std::endl;
+    nav_msgs::Odometry odom;
+    odom.header.frame_id = "camera_init";
+    odom.pose.pose.position.x = translation[0];
+    odom.pose.pose.position.y = translation[1];
+    odom.pose.pose.position.z = translation[2];
+    Eigen::Quaterniond q(rotation);
+    odom.pose.pose.orientation.w = q.w();
+    odom.pose.pose.orientation.x = q.x();
+    odom.pose.pose.orientation.y = q.y();
+    odom.pose.pose.orientation.z = q.z();
+    pubOdomAftMapped.publish(odom);
+    loop.sleep();
+    cloudInd++;
+    double mean_descriptor_time = std::accumulate(descriptor_time.begin(), descriptor_time.end(), 0) * 1.0 / descriptor_time.size();
+    double mean_query_time = std::accumulate(querying_time.begin(), querying_time.end(), 0) * 1.0 / querying_time.size();
+    double mean_update_time = std::accumulate(update_time.begin(), update_time.end(), 0) * 1.0 / update_time.size();
+    std::cout << "Total key frame number:" << keyCloudInd << ", loop number:" << triggle_loop_num << std::endl;
+    std::cout << "Mean time for descriptor extraction: " << mean_descriptor_time << "ms, query: " << mean_query_time << "ms, update: " << mean_update_time
+              << "ms, total: " << mean_descriptor_time + mean_query_time + mean_update_time << "ms" << std::endl;
+  }
 
   return 0;
 }
